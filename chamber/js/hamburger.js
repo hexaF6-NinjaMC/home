720b1376b57deebf3a18ce451daa5b9a1572eee3
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2023-07-26 14:29:16 -0500

    Delete chamber directory

--- chamber/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    document.getElementsByClassName('navigation')[0].classList.toggle('responsive')
-    
-    if (document.getElementsByClassName('responsive')[0]) {
-        document.getElementsByClassName('toggler')[0].innerHTML = '&#10006; CLOSE';
-    } else {
-        document.getElementsByClassName('toggler')[0].innerHTML = '&#9776; MENU';
-    }
-}
