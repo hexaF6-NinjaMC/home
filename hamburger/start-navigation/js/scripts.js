139a3f6728b56bd8d4759a924ed37f9741a1e0d0
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2023-07-26 14:29:49 -0500

    Delete hamburger/start-navigation directory

--- hamburger/start-navigation/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    let temp = document.getElementsByClassName('toggler')[0];
-    temp.classList.toggle('hide');
-    if (temp.classList[1] == 'hide') {
-        document.querySelector('.toggler > a').innerHTML = "&#9776; Menu";
-    } else {
-        document.querySelector('.toggler > a').innerHTML = "&#10006; Close";
-    }
-}
