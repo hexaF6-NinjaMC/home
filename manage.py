0c07f7c19de99496d441b7575dbe364205f06cf4
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2023-08-17 09:43:48 -0500

    added flask stuff

--- manage.py
+++ manage.py
@@ -1,1 +1,9 @@
+from flask import Flask
+application = Flask(__name__)
 
+@application.route('/')
+def homepage():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    application.run(debug=True, use_reloader=True)
